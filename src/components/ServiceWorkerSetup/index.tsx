a1d3977491f3d9130d1e96eb32ad977c7702295b
Brandon Cohen <brandon@z3hn.dev>
2025-06-26 17:45:03 -0700

    fix: add push subscription verification and resubscribe logic (#4163)
    * fix: verify webpush through the service worker
    
    * fix: prevent permissions check outside of settings
    
    * fix: do not resubscribe if no subscription exists
    
    refactor: added comments for verification
    
    * fix: prevent multiple backend calls
    
    refactor: rename variable for clarity
    
    * fix: subscriptions deleting properly
    
    refactor: move one line of code
    
    fix: add dependency in serviceworker
    
    * fix: prevent deletion of active subscription
    
    fix: add lock icon for better clarity
    
    fix: add i18n language
    
    * fix: convert subscription to json more reliably
    
    refactor: move from consoles to throws
    
    refactor: add error to throw
    
    refactor: remove unnecessary if statement
    
    * refactor: add push subscription function finder
    
    * fix: push active subscription to the top

--- src/components/ServiceWorkerSetup/index.tsx
+++ src/components/ServiceWorkerSetup/index.tsx
@@ -1,10 +1,14 @@
 /* eslint-disable no-console */
 
+import useSettings from '@app/hooks/useSettings';
 import { useUser } from '@app/hooks/useUser';
+import { verifyAndResubscribePushSubscription } from '@app/utils/pushSubscriptionHelpers';
 import { useEffect } from 'react';
 
 const ServiceWorkerSetup = () => {
   const { user } = useUser();
+  const { currentSettings } = useSettings();
+
   useEffect(() => {
     if ('serviceWorker' in navigator && user?.id) {
       navigator.serviceWorker
@@ -14,12 +18,44 @@ const ServiceWorkerSetup = () => {
             '[SW] Registration successful, scope is:',
             registration.scope
           );
+
+          // Do not prompt for permissions as we will handle this in the settings.
+          if (Notification.permission !== 'granted') {
+            console.warn(
+              '[SW] Push permissions not granted — skipping resubscribe'
+            );
+            return;
+          }
+
+          const subscription = await registration.pushManager.getSubscription();
+          // Bypass resubscribing if we have manually disabled the subscription
+          if (!subscription) {
+            return;
+          }
+
+          console.log(
+            '[SW] Existing push subscription:',
+            subscription.endpoint
+          );
+
+          const verified = await verifyAndResubscribePushSubscription(
+            user.id,
+            currentSettings
+          );
+
+          if (verified) {
+            console.log('[SW] Push subscription verified or refreshed.');
+          } else {
+            console.warn(
+              '[SW] Push subscription verification failed or not available.'
+            );
+          }
         })
         .catch(function (error) {
           console.log('[SW] Service worker registration failed, error:', error);
         });
     }
-  }, [user]);
+  }, [currentSettings, user]);
   return null;
 };
 
